5c710ec367123e94bab904772d17fe25495ccd53
Mertcan <medayi@Mertcan.localdomain>
2024-11-09 09:56:42 +0300

    libft için ilk benim için milyonuncu adım

--- ft_split.c
+++ ft_split.c
@@ -6,82 +6,83 @@
 /*   By: medayi <medayi@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 17:31:12 by medayi            #+#    #+#             */
-/*   Updated: 2024/11/07 20:22:39 by medayi           ###   ########.fr       */
+/*   Updated: 2024/11/09 09:56:10 by medayi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
-static size_t	count_words(const char *str, char c)
+static int	count_words(const char *s, char c)
 {
-	size_t	i;
-	size_t	count;
+	int	count = 0;
+	int	in_substring = 0;
 
-	i = 0;
-	count = 0;
-	while (str[i])
+	while (*s)
 	{
-		while (str[i] == c)
-			i++;
-		if (str[i] != '\0')
+		if (*s != c && !in_substring)
+		{
+			in_substring = 1;
 			count++;
-		while (str[i] && str[i] != c)
-			i++;
+		}
+		else if (*s == c)
+			in_substring = 0;
+		s++;
 	}
 	return (count);
 }
 
-static void	free_split(char **str, size_t words)
+static char	*word_dup(const char *start, size_t len)
 {
-	size_t	i;
+	char	*word;
 
-	i = 0;
-	while (i < words)
-		free(str[i++]);
-	free(str);
+	word = (char *)malloc(sizeof(char) * (len + 1));
+	if (!word)
+		return (NULL);
+	word[len] = '\0';
+	while (len--)
+		word[len] = start[len];
+	return (word);
 }
 
-static int	split_helper(char ***str, char const *s, t_size_list *a)
+static void	free_words(char **words, int count)
 {
-	(*str)[a->word] = ft_substr(s, a->start, a->end - a->start);
-	if (!(*str)[a->word])
-		return (0);
-	(a->word)++;
-	a->start = a->end;
-	return (1);
-}
-
-static void	init_size_list(t_size_list *a)
-{
-	a->start = 0;
-	a->end = 0;
-	a->word = 0;
+	while (count--)
+		free(words[count]);
+	free(words);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char		**str;
-	t_size_list	a;
+	char	**result;
+	int		words;
+	int		i = 0;
+	size_t	len;
 
-	init_size_list(&a);
 	if (!s)
 		return (NULL);
-	init_size_list(&a);
-	if (!s)
+	words = count_words(s, c);
+	result = (char **)malloc(sizeof(char *) * (words + 1));
+	if (!result)
 		return (NULL);
-	str = (char **)ft_calloc((count_words(s, c) + 1), sizeof(char *));
-	if (!str)
-		return (NULL);
-	while (s[a.start])
+	while (*s)
 	{
-		while (s[a.start] == c)
-			a.start++;
-		a.end = a.start;
-		while (s[a.end] && s[a.end] != c)
-			a.end++;
-		if (a.start < a.end)
-			if (!split_helper(&str, s, &a))
-				return (free_split(str, a.word), NULL);
+		if (*s != c)
+		{
+			len = 0;
+			while (s[len] && s[len] != c)
+				len++;
+			result[i] = word_dup(s, len);
+			if (!result[i++])
+			{
+				free_words(result, i - 1);
+				return (NULL);
+			}
+			s += len;
+		}
+		else
+			s++;
 	}
-	return (str);
+	result[i] = NULL;
+	return (result);
 }
